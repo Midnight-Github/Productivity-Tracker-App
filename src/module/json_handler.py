d9b7d23befdbb76b707e005524654aa886a192f6
!Midnight <kevinbinoj14@gmail.com>
2025-04-05 17:03:14 +0530

    fix file not found error, if data folder is missing

--- src/module/json_handler.py
+++ src/module/json_handler.py
@@ -1,8 +1,17 @@
 import json
+import os
 
 class JsonHandler:
-    def __init__(self, json_path):
+    def __init__(self, json_path, defauld_json):
         self.json_path = json_path
+
+        directory = os.path.dirname(self.json_path)
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
+        if not os.path.exists(self.json_path):
+            with open(self.json_path, 'w') as file:
+                json.dump(defauld_json, file, indent=4)
 
     def load(self):
         with open(self.json_path, 'r') as file:
@@ -13,4 +22,10 @@ class JsonHandler:
         with open(self.json_path, 'w') as file:
             json.dump(data, file, indent=4)
 
-user_json_handler = JsonHandler('src/data/user.json')
+user_json_handler = JsonHandler(
+    json_path='src/data/user.json',
+    defauld_json={
+        "accounts": [],
+        "latest_user": "None",
+        "auto_login": False,
+    })
